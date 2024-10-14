db3b1e887bac6d2006ce3f1aa7f0e58e67a43a82
tmarcorf <tmarco783@gmail.com>
2024-10-14 17:53:11 -0300

    feat: adicionando contratos dos repositórios

---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<Product> GetBydIdAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+
+        Task<Product> UpdateAsync(Product product);
+
+        Task<Product> RemoveAsync(Product product);
+    }
+}
