82092c60df0b6aca4e5e56967e7db6df31019497
tmarcorf <tmarco783@gmail.com>
2024-10-23 18:44:41 -0300

    feat: adicionando AutoMapper e DTOs para os conceitos de Product e Category

---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(100)]
+        [Required(ErrorMessage = "The name is required")]
+        public string Name { get; set; }
+    }
+}
