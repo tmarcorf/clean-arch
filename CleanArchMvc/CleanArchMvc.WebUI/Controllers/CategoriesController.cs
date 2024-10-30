6938f13a3b0141168405a1baf03c5432740bfa04
tmarcorf <tmarco783@gmail.com>
2024-10-29 23:29:36 -0300

    feat: adicionando recursos para edição e remoção do conceito Category

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -71,5 +71,37 @@ namespace CleanArchMvc.WebUI.Controllers
 
             return View(categoryDto);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
     }
 }
