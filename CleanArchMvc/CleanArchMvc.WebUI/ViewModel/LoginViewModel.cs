999847d6959ea94ad9396673e0c7a98ba26d2d0e
tmarcorf <tmarco783@gmail.com>
2024-11-12 13:56:38 -0300

    feat: adicionando implementação do serviço de autenticação

---
+++ CleanArchMvc/CleanArchMvc.WebUI/ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebUI.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid format email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
